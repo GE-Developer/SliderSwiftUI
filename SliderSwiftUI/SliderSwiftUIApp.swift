ada5cf6a290fefbcf964fe9d220b0ea96df4dd1f
GE-Developer <on-line-misha@mail.ru>
2023-06-11 23:18:45 +0400

    Initial Commit

---
+++ SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by M I C H A E L on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
